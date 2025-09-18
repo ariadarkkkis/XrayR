279548c4a8fe27f26b1cb2b4928684bcb34a4370
ariadarkkkis <fresh1995boy@gmail.com>
2025-09-18 12:52:30 +0330

    feat: update commands based on xray commands

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
-	"github.com/ariadarkkkis/XrayR/cmd"
-	log "github.com/sirupsen/logrus"
+	"os"
+
+	_ "github.com/ariadarkkkis/XrayR/cmd"
+	"github.com/xtls/xray-core/main/commands/base"
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		log.Fatal(err)
+	// Check if we have any arguments
+	if len(os.Args) == 1 {
+		// No arguments - run XrayR with default behavior
+		base.RootCommand.Run(base.RootCommand, []string{})
+		return
 	}
+
+	// Check if first argument starts with dash (flag) - run XrayR
+	if len(os.Args) > 1 && os.Args[1][0] == '-' {
+		// Parse flags manually and run XrayR
+		base.RootCommand.Flag.Parse(os.Args[1:])
+		base.RootCommand.Run(base.RootCommand, base.RootCommand.Flag.Args())
+		return
+	}
+
+	// Otherwise use normal command execution
+	base.Execute()
 }
